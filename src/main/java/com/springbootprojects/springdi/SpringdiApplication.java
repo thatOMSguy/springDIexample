ca478fb8126680d542d9313cc8936fad31b08349
Ravi Kumar <84226388+thatomsguy@users.noreply.github.com>
2023-03-23 11:40:38 +0530

    First Commit

---
+++ src/main/java/com/springbootprojects/springdi/SpringdiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.springdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdiApplication.class, args);
+	}
+
+}
