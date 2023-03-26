b596439b80a188991b21315210eb215fef71c752
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-03-26 16:24:03 +0530

    Test success run for when ES is Active profile

--- src/test/java/com/springbootprojects/springdi/controllers/i18n/Myi18nControllerTestES.java
+++ src/test/java/com/springbootprojects/springdi/controllers/i18n/Myi18nControllerTestES.java
@@ -1,4 +1,22 @@
 package com.springbootprojects.springdi.controllers.i18n;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("ES")
+@SpringBootTest
 public class Myi18nControllerTestES {
+
+    @Autowired
+    Myi18nController myi18nController;
+
+    @Test
+    void sayHelloTest() {
+
+        System.out.println(myi18nController.sayHello());
+
+    }
 }
