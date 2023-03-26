98eb2b0bf1bf39f996d79b8c50a8999859ec7229
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-03-26 16:21:20 +0530

    Profile annotation added Test success run for when EN is Active profile

--- src/main/java/com/springbootprojects/springdi/services/i18n/SpanishGreetingService.java
+++ src/main/java/com/springbootprojects/springdi/services/i18n/SpanishGreetingService.java
@@ -1,9 +1,11 @@
 package com.springbootprojects.springdi.services.i18n;
 
 import com.springbootprojects.springdi.services.GreetingService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 
+@Profile("ES")
 @Service("i18nService")
 public class SpanishGreetingService implements GreetingService {
     @Override
